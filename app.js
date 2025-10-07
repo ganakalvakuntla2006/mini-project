6e7c8b2767ac28466b929b98a18ea7369356d569
ganakalvakuntla2006 <ganakalvakuntla@gmail.com>
2025-10-07 13:27:33 +0530

    formcreation

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const userModel = require("./models/user");
+const cookieParser = require('cookie-parser');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.send("hey");
+    res.render("index");
 });
 
- app.listen(3000);
+app.listen(3000);
