8ccde7e70afe4dadad206ccbfaf3600438db0ce6
ganakalvakuntla2006 <ganakalvakuntla@gmail.com>
2025-10-07 14:50:12 +0530

    formupdate

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.render("index");
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
