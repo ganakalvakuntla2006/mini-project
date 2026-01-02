64361af60bd0638b08586f80cd2b025fc558d3fe
ganakalvakuntla2006 <ganakalvakuntla@gmail.com>
2026-01-02 14:37:12 +0530

    logout

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.post('/register', async(req, res) => {
     console.log(req.body);
     let user= await userModel.findOne({email});
     if(user) return res.status(400).send("user already registered");
-    
+     
 
     bcrypt.genSalt(10,(err,salat)=>{
         bcrypt.hash(password,salat,async(err,hash)=>{
@@ -45,24 +45,34 @@ app.post('/login', async(req, res) => {
     let {email,password,username,name,age}=req.body;
     console.log(req.body);
     let user= await userModel.findOne({email});
-    if(user) return res.status(400).send("user already registered");
+    if(!user) return res.status(500).send("something went wrong");
     
 
-    bcrypt.genSalt(10,(err,salat)=>{
-        bcrypt.hash(password,salat,async(err,hash)=>{
-            let user = await userModel.create({
-                username,
-                email,
-                age,
-                name,
-                password: hash
-            })
-            let token =jwt.sign({email:email,userid:user._id},"shhh")
-            res.cookie("token",token);
-            res.send("register")
-        })
+    // bcrypt.genSalt(10,(err,salat)=>{
+    //     bcrypt.hash(password,salat,async(err,hash)=>{
+    //         let user = await userModel.create({
+    //             username,
+    //             email,
+    //             age,
+    //             name,
+    //             password: hash
+    //         })
+    //         let token =jwt.sign({email:email,userid:user._id},"shhh")
+    //         res.cookie("token",token);
+    //         res.send("register")
+    //     })
+    //})
+    bcrypt.compare(password,user.password,function(err,result){
+        if(result) res.status(200).send("you can login");
+        else res.redirect("/login");
     })
 });
+
+app.get('/logout',(req,res)=>{
+    res.cookie("token","");
+    res.redirect("/login");
+})
+
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
 });
