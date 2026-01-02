f1c591f5e7f9f0ae6ee6725e47ba0b9773c266a8
ganakalvakuntla2006 <ganakalvakuntla@gmail.com>
2026-01-02 13:59:17 +0530

    login

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const userModel = require("./models/user");
+const postModel = require("./models/post");
 const cookieParser = require('cookie-parser');
+const bcrypt=require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -11,7 +14,56 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
     res.render("index");
 });
+app.get('/login',(req,res)=>{
+    res.render("login");
+})
+// console.log(userModel);
+app.post('/register', async(req, res) => {
+    let {email,password,username,name,age}=req.body;
+    console.log(req.body);
+    let user= await userModel.findOne({email});
+    if(user) return res.status(400).send("user already registered");
+    
 
+    bcrypt.genSalt(10,(err,salat)=>{
+        bcrypt.hash(password,salat,async(err,hash)=>{
+            let user = await userModel.create({
+                username,
+                email,
+                age,
+                name,
+                password: hash
+            })
+            let token =jwt.sign({email:email,userid:user._id},"shhh")
+            res.cookie("token",token);
+            res.send("register")
+        })
+    })
+});
+
+app.post('/login', async(req, res) => {
+    let {email,password,username,name,age}=req.body;
+    console.log(req.body);
+    let user= await userModel.findOne({email});
+    if(user) return res.status(400).send("user already registered");
+    
+
+    bcrypt.genSalt(10,(err,salat)=>{
+        bcrypt.hash(password,salat,async(err,hash)=>{
+            let user = await userModel.create({
+                username,
+                email,
+                age,
+                name,
+                password: hash
+            })
+            let token =jwt.sign({email:email,userid:user._id},"shhh")
+            res.cookie("token",token);
+            res.send("register")
+        })
+    })
+});
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
 });
+
